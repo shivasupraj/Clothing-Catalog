cee17384e17f4efba8bbd53d2deee6d392949b0a
shivasupraj <shivasupraj.p@gmail.com>
2018-12-31 17:51:29 -0600

    Initial commit

---
+++ models.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(80), nullable = True)
+    picture = Column(String(256), nullable = True)
+    password_hash = Column(String(60))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+    
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture,
+        }
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    name = Column(String(50), nullable = False)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'user_id' : self.user_id,
+        }
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    name = Column(String(50), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(1000))
+    picture = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'picture' : self.picture,
+            'category_id' : self.category_id,
+            'user_id' : self.user_id,
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
---
+++ views.py
@@ -0,0 +1,38 @@
+from models import Base, User
+from flask import Flask, jsonify, request, url_for, abort, g, render_template
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+from sqlalchemy import create_engine
+
+from flask_httpauth import HTTPBasicAuth
+import json
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
+
+auth = HTTPBasicAuth()
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.bind = engine
+session = scoped_session(sessionmaker(bind=engine))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to main page'
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    app.run(host='0.0.0.0', port=5000)
